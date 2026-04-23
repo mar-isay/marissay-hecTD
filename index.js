91a0d55a17abd773bc2db1e6b4d233c72d7d7f4f
mar-isay <missaabakar3@gmail.com>
2026-04-23 03:57:57 +0300

    4. asama: Express sunucusu lokalde calistirildi

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('HEC TD: Sunucu Basariyla Calisiyor!');
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} adresinde hazir.`);
+});
